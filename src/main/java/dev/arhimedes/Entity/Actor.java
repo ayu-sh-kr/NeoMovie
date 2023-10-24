cdb9cf6ce5c68d6f08fc4e1e80cc0ad7e3f40361
ayu-sh-kr <akjaiswal2003@gmail.com>
2023-10-24 15:58:47 +0530

    Added relationship with nodes

--- src/main/java/dev/arhimedes/Entity/Actor.java
+++ src/main/java/dev/arhimedes/Entity/Actor.java
@@ -11,15 +11,44 @@ public class Actor {
     @Id
     public final String name;
 
-    @Relationship("ACTED_IN")
-    public final List<Movie> movies;
+    public final Integer born;
 
-    @Relationship("DIRECTED")
-    public final List<Movie> movieList;
+    @Relationship(type = "ACTED_IN", direction = Relationship.Direction.OUTGOING)
+    private final List<Movie> actedIn;
 
-    public Actor(String name, List<Movie> movies, List<Movie> movieList) {
+    @Relationship(type = "DIRECTED", direction = Relationship.Direction.OUTGOING)
+    private final List<Movie> directedIn;
+
+
+
+    public Actor(String name, Integer born, List<Movie> actedIn, List<Movie> directedIn) {
         this.name = name;
-        this.movies = movies;
-        this.movieList = movieList;
+        this.born = born;
+        this.actedIn = actedIn;
+        this.directedIn = directedIn;
+    }
+
+    @Override
+    public String toString() {
+        return "Actor{" +
+                "name='" + name + '\'' +
+                ", born=" + born +
+                '}';
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Integer getBorn() {
+        return born;
+    }
+
+    public List<Movie> getActedIn() {
+        return actedIn;
+    }
+
+    public List<Movie> getDirectedIn() {
+        return directedIn;
     }
 }
