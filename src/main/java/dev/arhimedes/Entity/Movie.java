96b9911d064343bf1c209d0886cfb6d76b7dda6d
ayu-sh-kr <akjaiswal2003@gmail.com>
2023-10-24 16:02:56 +0530

    Represent tha data node

--- src/main/java/dev/arhimedes/Entity/Movie.java
+++ src/main/java/dev/arhimedes/Entity/Movie.java
@@ -2,15 +2,26 @@ package dev.arhimedes.Entity;
 
 import org.springframework.data.neo4j.core.schema.Id;
 import org.springframework.data.neo4j.core.schema.Node;
+import org.springframework.data.neo4j.core.schema.Relationship;
+
+import java.util.List;
 
 @Node
 public class Movie {
     @Id
     public final String title;
 
-    public Movie(String title) {
+    public String tagline;
+
+    public Integer released;
+
+
+    public Movie(String title, String tagline, Integer released) {
         this.title = title;
+        this.tagline = tagline;
+        this.released = released;
     }
+
 
     @Override
     public String toString() {
