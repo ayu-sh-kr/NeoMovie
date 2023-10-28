35ca0e1ad66df72046b828682c6630be7648d188
ayu-sh-kr <akjaiswal2003@gmail.com>
2023-10-28 23:05:58 +0530

    upgrading our user to userdetails so that it can be return in uds class

---
+++ src/main/java/dev/arhimedes/service/CustomUserDetails.java
@@ -0,0 +1,54 @@
+package dev.arhimedes.service;
+
+import dev.arhimedes.Entity.User;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class CustomUserDetails implements UserDetails {
+    private User user;
+    private List<GrantedAuthority> grantedAuthorities;
+
+    public CustomUserDetails(User user){
+        this.user = user;
+    }
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return grantedAuthorities = user.getAuthorityCollection().stream().map(authority -> new SimpleGrantedAuthority(authority.toString())).collect(Collectors.toList());
+    }
+
+    @Override
+    public String getPassword() {
+        return user.getPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        return user.getEmail();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
