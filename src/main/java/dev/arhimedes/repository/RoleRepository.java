fe4a196d2f395e569dbeef023f44a685dbc3d6cd
ayu-sh-kr <akjaiswal2003@gmail.com>
2023-10-29 17:19:58 +0530

    added custom query

--- src/main/java/dev/arhimedes/repository/RoleRepository.java
+++ src/main/java/dev/arhimedes/repository/RoleRepository.java
@@ -2,8 +2,13 @@ package dev.arhimedes.repository;
 
 import dev.arhimedes.Entity.Role;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface RoleRepository extends JpaRepository<Role, Long> {
     Role findByName(String name);
+
+    @Query(value = "SELECT COUNT(name) FROM roles where name = MANAGERS", nativeQuery = true)
+    int getManagers();
+
 
 }
--- src/main/java/dev/arhimedes/repository/UserRepository.java
+++ src/main/java/dev/arhimedes/repository/UserRepository.java
@@ -2,8 +2,12 @@ package dev.arhimedes.repository;
 
 import dev.arhimedes.Entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface UserRepository extends JpaRepository<User, Long> {
     User findByEmail(String email);
     Boolean existsByEmail(String email);
+
+    @Query(value = "SELECT COUNT(name) FROM user_details ", nativeQuery = true)
+    int getCount();
 }
