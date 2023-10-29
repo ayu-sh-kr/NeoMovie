135834d8532df93474de6e4bfaff953b2e7fbfa9
ayu-sh-kr <akjaiswal2003@gmail.com>
2023-10-29 17:23:14 +0530

    refactoring for auth based verification

--- src/main/java/dev/arhimedes/config/SecurityConfig.java
+++ src/main/java/dev/arhimedes/config/SecurityConfig.java
@@ -2,12 +2,8 @@ package dev.arhimedes.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.ProviderManager;
-import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -22,9 +18,9 @@ public class SecurityConfig {
                 .csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(authorize -> {
                     authorize.requestMatchers("/resources/**", "/*.css", "/*.js", "/images/*.*", "/javascript/*.*").permitAll();
-                    authorize.requestMatchers("/admin").hasRole("ADMIN");
                     authorize.requestMatchers("/login").permitAll();
                     authorize.requestMatchers("/register").permitAll();
+                    authorize.requestMatchers("/admin", "/admin/**").hasAuthority("ADMIN");
                     authorize.anyRequest().authenticated();
                 })
                 .formLogin(form -> {
