1b43c22832c769daed69fd140ff74090b0711ab6
ayu-sh-kr <akjaiswal2003@gmail.com>
2023-10-31 01:41:11 +0530

    javascript to manage admin home page

--- src/main/resources/static/javascript/adminDashboard.js
+++ src/main/resources/static/javascript/adminDashboard.js
@@ -1,4 +1,4 @@
-const active = document.getElementById('activeUser');
+const activeUser = document.getElementById('activeUser');
 const totalUser = document.getElementById('totalUser');
 const managers = document.getElementById('managers');
 const entryPoint = document.getElementById('entryPoint');
@@ -15,29 +15,37 @@ function setData(uri){
         .then(data => {
             console.log(data)
             totalUser.innerText = data["Total User"];
+            activeUser.innerText = data["Active User"];
+            managers.innerText = data["Managers"];
         })
-        .catch(error => console.log(error))
+        .catch(error => console.log(error));
 }
 
-function getUsers(){
+function getUsers(text){
     freeResultBox(entryPoint);
-    let uri = "http://localhost:8080/admin/findAll";
-    fetchUsers(uri);
+    let uri = "http://localhost:8080/admin/";
+    if(text === 'totalUsers')
+        fetchUsers(uri + 'findAll');
+    else if(text === 'activeUsers')
+        fetchUsers(uri + 'activeUsers')
+    else if(text === 'managers')
+        fetchUsers(uri + 'managers');
 }
 
 function fetchUsers(uri){
     fetch(uri)
         .then(response => response.json())
         .then(data => {
-            console.log(data);
+            // console.log(data);
             data.forEach(value => {
                 role = getRoles(value.roleList);
                 const component =  `<tr class="border-y border-gray-200 dark:border-gray-700">
                     <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap bg-gray-50 dark:text-white dark:bg-gray-800">
-                    ${value.name}</th>
+                    <a href="/admin/userProfile?email=${value.email}">${value.name}</a></th>
                     <td class="px-6 py-4">${value.email}\n</td>
                     <td class="px-6 py-4 bg-gray-50 dark:bg-gray-800\">${role}</td>
                     <td class="px-6 py-4">${value.createdOn}</td>
+                    <td class="px-6 py-4 bg-gray-50 dark:bg-gray-800\">${value.active}</td>
                     </tr>`;
                 entryPoint.insertAdjacentHTML("beforeend", component);
             })
@@ -48,7 +56,7 @@ function fetchUsers(uri){
 function getRoles(value){
     str = '';
     for(let i = 0; i < value.length; i++){
-        console.log(value[i].name);
+        // console.log(value[i].name);
         str += value[i].name + ' ';
     }
     return str;
