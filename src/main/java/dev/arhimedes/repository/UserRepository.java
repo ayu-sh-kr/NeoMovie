749748edc89f9a0db8f7384dcd50ccbb9256789a
ayu-sh-kr <akjaiswal2003@gmail.com>
2023-10-31 01:46:08 +0530

    query to return active user and count of it

--- src/main/java/dev/arhimedes/repository/UserRepository.java
+++ src/main/java/dev/arhimedes/repository/UserRepository.java
@@ -4,10 +4,18 @@ import dev.arhimedes.Entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
+import java.util.List;
+
 public interface UserRepository extends JpaRepository<User, Long> {
     User findByEmail(String email);
     Boolean existsByEmail(String email);
 
     @Query(value = "SELECT COUNT(name) FROM user_details ", nativeQuery = true)
     int getCount();
+
+    @Query(value = "SELECT COUNT(account_active) FROM user_details WHERE account_active = 1;", nativeQuery = true)
+    int getActiveAccountCount();
+
+    @Query(value = "SELECT * FROM user_details WHERE account_active = 1;", nativeQuery = true)
+    List<User> getActiveUsers();
 }
