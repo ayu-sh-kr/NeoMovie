bec12d92dfb4bfd16e26ecc72bf17b2963780bc2
ayu-sh-kr <akjaiswal2003@gmail.com>
2023-10-31 01:45:35 +0530

    remove the syntax error for query

--- src/main/java/dev/arhimedes/repository/RoleRepository.java
+++ src/main/java/dev/arhimedes/repository/RoleRepository.java
@@ -7,8 +7,6 @@ import org.springframework.data.jpa.repository.Query;
 public interface RoleRepository extends JpaRepository<Role, Long> {
     Role findByName(String name);
 
-    @Query(value = "SELECT COUNT(name) FROM roles where name = MANAGERS", nativeQuery = true)
-    int getManagers();
-
-
+    @Query(value = "SELECT COUNT(name) FROM roles where name = 'MANAGER'", nativeQuery = true)
+    int getManagersCount();
 }
