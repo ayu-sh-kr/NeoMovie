909b227646fd1455628baff8ba765d7e5ccc7171
ayu-sh-kr <akjaiswal2003@gmail.com>
2023-11-02 22:17:09 +0530

    Enabled scheduling to clear repository regularly for email verification tokens

--- src/main/java/dev/arhimedes/NeoMovieApplication.java
+++ src/main/java/dev/arhimedes/NeoMovieApplication.java
@@ -3,8 +3,10 @@ package dev.arhimedes;
 import dev.arhimedes.repository.ActorRepository;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication(scanBasePackages = "dev.arhimedes")
+@EnableScheduling
 public class NeoMovieApplication{
 
     private final ActorRepository actorRepository;
